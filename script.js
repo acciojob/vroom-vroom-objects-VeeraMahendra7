42b925c823fc9f4ea090d2551f4afcd8a26a01b8
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-16 09:16:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,6 +21,5 @@ SportsCar.prototype.getTopSpeed = function(){
 	return this._topSpeed;
 }
 
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-console.log(car.getMakeModel());
-console.log(car.getTopSpeed());
+window.Car = Car;
+window.SportsCar = SportsCar;
